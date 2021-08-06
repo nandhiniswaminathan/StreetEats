5abdf9711e75de6c1bacfbf3e0a07924f6c62224
Joey <camph@sheridancollege.ca>
2021-08-06 12:20:29 -0400

    Update .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,32 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# change password to parameter using .env variables
 app.config[ "SQLALCHEMY_DATABASE_URI" ] = "postgresql://postgres:pass@localhost:5432/streeteatsdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-# create user table
+friends = db.Table('friends',
+    db.Column("user_id_fk", db.Integer, db.ForeignKey("users.user_id"), primary_key=True),
+    db.Column("friend_id", db.Integer, db.ForeignKey("users.user_id"), primary_key=True)
+)
+'''To override the table name, set the __tablename__ class attribute.'''
+# creates user table
 class UserModel(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String())
     password = db.Column(db.String())
-    #todo add column for dietary restrictions, location, lists, friend list,
-
+    friendship = db.relationship("UserModel",
+                    secondary=friends,
+                    primaryjoin=user_id==friends.c.user_id_fk,
+                    secondaryjoin=user_id==friends.c.friend_id,
+                    backref="followed_by"
+    )
+    # friendships = db.relationship('users', backref='user', lazy=True)
+        
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -26,9 +38,29 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
+class ListModel(db.Model):
+    __tablename__ = "lists"
+
+    list_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    listname = db.Column(db.String())
+    user_id_fk = db.Column(db.String())
+
+    def __init__(self, listname, user_id_fk):
+        self.listname = listname
+        self.user_id_fk = user_id_fk
+
+    def __repr__(self):
+        return f"<User {self.listname}>"
+
+
+# class FriendModel(db.Model):
+#     ___tablename___ = "friends"
+
+#     user_id_fk = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+#     friend_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
 load_dotenv()
 db.create_all()
-
 
 @app.route("/")
 def index():
